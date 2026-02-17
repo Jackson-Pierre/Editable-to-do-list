472575e4d152bbc4898cf6449820eef29fa8eceb
Jackson <jacksonppereira4@gmail.com>
2026-02-17 18:08:29 -0400

    area de tasks pronto

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const Theme = document.querySelector(".theme-toggle");
 const bgImage = document.querySelector(".bg");
+const bgDesktop = document.querySelector(".bg-desktop");
 
 function changeTheme(){
     document.body.classList.toggle("white-theme");
-    bgImage.src.includes("bg-desktop-light") ? 
-    bgImage.src = "images/bg-desktop-dark.jpg" : 
-    bgImage.src = "images/bg-desktop-light.jpg";
+    bgImage.src.includes("bg-mobile-light") ? 
+    bgImage.src = "images/bg-mobile-dark.jpg" : 
+    bgImage.src = "images/bg-mobile-light.jpg";
+    bgDesktop.srcset.includes("bg-desktop-light") ? 
+    bgDesktop.srcset = "images/bg-desktop-dark.jpg" : 
+    bgDesktop.srcset = "images/bg-desktop-light.jpg";
 }
 
 Theme.addEventListener("click", changeTheme)
